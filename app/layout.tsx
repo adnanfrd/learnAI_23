11c29d52100ad5452989a7e34d730363c6c94bc7
Umar Mughal <muhammad.umar@apkamuaalij.com>
2025-10-16 12:04:22 +0500

    resolve build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,24 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import ClientLayoutWrapper from "../components/ClientLayoutWrapper";
 import { SupportProvider } from "@/components/SupportProvider";
 
-const inter = Inter({ subsets: ["latin"] });
-
+const inter = localFont({
+  src: [
+    {
+      path: "../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+      weight: "100 900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Inter-Italic-VariableFont_opsz,wght.ttf",
+      weight: "100 900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-inter",
+});
 export const metadata: Metadata = {
   title: "JobEscape - Build & Sell AI Agents | No experience required",
   description:
